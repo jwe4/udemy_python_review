a6fa6fa429f578cd315ad12ca1bcf14faa4131da
jwe4 <jeweaver@gmail.com>
2017-07-23 20:59:28 -0500

    works through getting access token

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'pBqwEDsbjiogFSzgayFBu4jDr'
+CONSUMER_SECRET = '45FFH7QJ9oJJwMx4vkEUQlQZJxsDibb5xHaPQMqxaNpIjz7e0Z'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
---
+++ login.py
@@ -0,0 +1,31 @@
+import constants
+import oauth2
+import urllib.parse as urlparse
+
+consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+client = oauth2.Client(consumer)
+
+response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+
+if response.status != 200:
+    print('An error occurred getting the request token from Twitter!')
+
+request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+# www.oursite.com 'login with twitter button'
+#they presss 'sign in' or 'authorize
+# Twitter sends them back to e.g. www.ourwebsite.com/auth
+# we get that auth code + request token -> twitter -> access token
+
+# don't have web site yet so get a token
+print("Go to following site in your browser:")
+print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']));
+
+oauth_verifier = input("What is the PIN? ")
+
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+print(access_token)
