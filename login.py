107580c97a56f9bb0dcb5b1e776c54578be9af20
jwe4 <jeweaver@gmail.com>
2017-07-25 17:40:52 -0500

    access tweets with db stored oauth info

--- login.py
+++ login.py
@@ -3,56 +3,70 @@ import oauth2
 import urllib.parse as urlparse
 import json
 from user import User
+from database import Database
 
 
-# Create a consumer, which uses CONSUMER_KEY and CONSUMER_SECRET to identify our app uniquely
 consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
-client = oauth2.Client(consumer)
+Database.initialise(user='postgres',
+                    password='12345',
+                    database='learning2',
+                    host='localhost')
+
+user_email = input("Enter your e-mail address: ")
+user = User.load_from_db_by_email(user_email)
+if user:
+    pass
+else:
+    # Create a consumer, which uses CONSUMER_KEY and CONSUMER_SECRET to identify our app uniquely
+    client = oauth2.Client(consumer)
 
 
-# Uset the client to perform a request for the request token
-response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+    # Uset the client to perform a request for the request token
+    response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
 
-if response.status != 200:
-    print('An error occurred getting the request token from Twitter!')
-
-
-# Get the request token parsing the query string returned
-request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
-
-# www.oursite.com 'login with twitter button'
-#they presss 'sign in' or 'authorize
-# Twitter sends them back to e.g. www.ourwebsite.com/auth
-# we get that auth code + request token -> twitter -> access token
-
-# don't have web site yet so get a token
-
-# Ask the user to authorize our app and give us the pin code
-print("Go to following site in your browser:")
-print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']));
-
-oauth_verifier = input("What is the PIN? ")
+    if response.status != 200:
+        print('An error occurred getting the request token from Twitter!')
 
 
-# Create a Token object which contains the request token and the verifier
-token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
+    # Get the request token parsing the query string returned
+    request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
-# Create a client with our consumer (our app) and the newly created (and verified) token
-client = oauth2.Client(consumer, token)
+    # www.oursite.com 'login with twitter button'
+    #they presss 'sign in' or 'authorize
+    # Twitter sends them back to e.g. www.ourwebsite.com/auth
+    # we get that auth code + request token -> twitter -> access token
 
-# Ask Twitter for an access token, and Twitter knws it should gus it becuase we've verified the request token
-response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
-access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
-print(access_token)
+    # don't have web site yet so get a token
 
-user =  User('jimWvr@yahoo.com', 'jim', 'weaver',access_token['oauth_token'],access_token['oauth_token_secret'] )
-user.save_to_db();
+    # Ask the user to authorize our app and give us the pin code
+    print("Go to following site in your browser:")
+    print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']));
+
+    oauth_verifier = input("What is the PIN? ")
+
+
+    # Create a Token object which contains the request token and the verifier
+    token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+
+    # Create a client with our consumer (our app) and the newly created (and verified) token
+    client = oauth2.Client(consumer, token)
+
+    # Ask Twitter for an access token, and Twitter knws it should gus it becuase we've verified the request token
+    response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+    access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+    print(access_token)
+
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    user = User(user_email, first_name, last_name, access_token['oauth_token'],access_token['oauth_token_secret'] )
+    user.save_to_db();
 
 
 
 # Create an 'authorized_token' Token object and use that to perform Twitter API calls on behalf oth the user
-authorized_token = oauth2.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
+authorized_token = oauth2.Token(user.oauth_token, user.oauth_token_secret)
 authorized_client = oauth2.Client(consumer, authorized_token)
 
 #Make Twitter API calls!
--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 from database import CursorFromConnectionFromPool
 
+
 class User:
     def __init__(self, email, first_name, last_name, oauth_token, oauth_token_secret, id):
         self.email = email
@@ -9,21 +10,21 @@ class User:
         self.oauth_token_secret = oauth_token_secret
         self.id = id
 
-
     def __repr__(self):
         return "<user: {}>".format(self.email)
 
     def save_to_db(self):
         with CursorFromConnectionFromPool() as cursor:
-            cursor.execute('insert into users (email,first_name, last_name, oauth_token, oauth_token_secret) values (%s,%s,%s,%s,%s)',
-                           (self.email, self.first_name, self.last_name, self.oauth_token, self.oauth_token_secret))
-
+            cursor.execute(
+                'insert into users (email,first_name, last_name, oauth_token, oauth_token_secret) values (%s,%s,%s,%s,%s)',
+                (self.email, self.first_name, self.last_name, self.oauth_token, self.oauth_token_secret))
 
     @classmethod
     def load_from_db_by_email(cls, email):
         with CursorFromConnectionFromPool() as cursor:
-            cursor.execute('select * from users where email=%s', (email,)) #needs a tuple
+            cursor.execute('select * from users where email=%s', (email,))  # needs a tuple
             user_data = cursor.fetchone()
-            return cls(email=user_data[1], first_name=user_data[2],
-                       last_name=user_data[3], oauth_token=user_data[4],
-                       oauth_token_secret=user_data[7], id=user_data[0])
+            if user_data:
+                return cls(email=user_data[1], first_name=user_data[2],
+                           last_name=user_data[3], oauth_token=user_data[4],
+                           oauth_token_secret=user_data[5], id=user_data[0])
