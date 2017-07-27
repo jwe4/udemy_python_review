e37dd194bea2082ce9576ed24d5db3e978bd36c2
jwe4 <jeweaver@gmail.com>
2017-07-27 11:23:00 -0500

    works through redirection to twitter

--- twitter_utils.py
+++ twitter_utils.py
@@ -21,9 +21,14 @@ def get_request_token():
 def get_oauth_verifier(request_token):
     # Ask the user to authorize our app and give us the pin code
     print("Go to following site in your browser:")
-    print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']));
+    print(get_oauth_verifier_url(request_token));
 
     return input("What is the PIN? ")
+
+
+def get_oauth_verifier_url(request_token):
+    return "{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token'])
+
 
 def get_access_token(request_token, oauth_verifier):
     # Create a Token object which contains the request token and the verifier
